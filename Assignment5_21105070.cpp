ae458b1e15ab7d7cb34d5f69e7b1ea8ea53c29c4
dhruv1080 <97087996+dhruv1080@users.noreply.github.com>
2022-11-11 00:08:18 +0530

    Update Assignment5_21105070.cpp

--- Assignment5_21105070.cpp
+++ Assignment5_21105070.cpp
@@ -2,140 +2,146 @@
 // ECE
 // 21105070
 
-/*
-[Ans.1]
-An "In-Place" sorting algorithm is algorithm whose space complexity
-lies between O(1) and O(logn) both included.
-We can say an in-place algorithm is an algorithm that does need a
-small extra space and produces an output in the same memory that
-contains the data by transforming the input ‘in-place’.Where as an
-"Out-Place" sorting algorithm is algorithm whose space complexity is
-greater than O(logn).
-*/
-
-//[Ans.2]
-
-// Insertion Sort Using In-Place Algorithm
 #include <iostream>
 using namespace std;
-// Creating Insertion Sort void function
-void insertion_sort_IP(int arr[], int n)
+
+/*
+QUESTION:
+Compare Bubble sort and Selection sort based on the following:
+1. Number of comparisons
+2. Number of swaps
+3. Inplace and Outplace implementations
+*/
+
+// SOLUTION
+
+// Function Implementing Bubble Sort Algorithm
+void bubble_sort(int arr[], int n)
 {
-    // Initialising variables i and j
-    int i = 0, j = i + 1;
-    while (j <= n - 1)
+    int i = 0, j = 1, t = n - 1;
+    int no_of_swap = 0;
+    int no_of_comp = 0;
+    while (t > 0)
     {
-        int temp = arr[j];
-        while (i >= 0)
+        while (j <= t)
         {
-            if (arr[i] > temp)
+            if (arr[i] > arr[j])
             {
-                arr[i + 1] = arr[i];
-                i--;
+                swap(arr[i], arr[j]);
+                no_of_swap++;
             }
-            if (i == -1)
-            {
-                arr[0] = temp;
-                break;
-            }
-            if (arr[i] <= temp)
-            {
-                arr[i + 1] = temp;
-                break;
-            }
+            i++, j++;
+            no_of_comp++;
         }
-        j++;
-        i = j - 1;
+        t--;
+        i = 0, j = 1;
     }
-    return;
+    cout << " . Number of SWAP:" << no_of_swap << endl;
+    cout << " . Number of COMPARISON:" << no_of_comp << endl;
+    cout << " . [INPLACE SORTING ALGORITHM USED]" << endl;
 }
 
-// Insertion Sort Using Out-Place Algorithm
-void insertion_sort_OP(int arr[], int si, int ei)
+// Function that returns index of minimum value in an array
+int min_ind(int arr[], int si, int ei)
+{
+    // Initialising minimum value as first element of array
+    int m = arr[si];  // m=minimum value in array
+    int min_ind = si; // min_ind=index holding minimum value
+    for (int i = si; i <= ei; i++)
+    {
+        int min_prev = m;
+        m = min(m, arr[i]);
+        if (m < min_prev)
+        {
+            min_ind = i;
+        }
+    }
+    return min_ind;
+}
+
+// Function Implementing Selection-Sort Algorithm
+void selection_sort(int arr[], int n)
 {
     // si=starting index
     // ei=ending index
-    if (si == ei)
+    int si = 0, ei = n - 1;
+    int no_of_swap = 0;
+    int no_of_comp = 0;
+    while (si < ei)
     {
-        return;
+        int mi = min_ind(arr, si, ei); // mi=index holding minimum value
+        no_of_comp = no_of_comp + (ei - si + 1);
+        if (si != mi)
+        {
+            swap(arr[si], arr[mi]);
+            no_of_swap++;
+        }
+        si++;
     }
-    int j = si + 1;
-    int temp = arr[j];
-    while (si >= 0)
-    {
-        if (arr[si] > temp)
-        {
-            arr[si + 1] = arr[si];
-            si--;
-        }
-        if (si == -1)
-        {
-            arr[0] = temp;
-            break;
-        }
-        if (arr[si] <= temp)
-        {
-            arr[si + 1] = temp;
-            break;
-        }
-    }
-    insertion_sort_OP(arr, si + 1, ei);
+    cout << " . Number of SWAP:" << no_of_swap << endl;
+    cout << " . Number of COMPARISON:" << no_of_comp << endl;
+    cout << " . [INPLACE SORTING ALGORITHM USED]" << endl;
 }
-// Function to print array
+
+// Function to print the array
 void pfun(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-}
-int main()
-{
-    int n;
-    // Taking Input array
-    cout << "ENTER NUMBER OF ELEMENTS IN THE ARRAY: ";
-    cin >> n;
-    int arr[n], arr2[n]; // Two array with same size and elements
-    cout << "ENTER ELEMENTS OF ARRAY SEPERATED BY SPACE: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    // copying arr to arr2
-    for (int i = 0; i < n; i++)
-    {
-        arr2[i] = arr[i];
-    }
-    cout << endl;
-    cout << "ARRAY BEFORE SORTING : ";
-    pfun(arr, n);
-    // Calling insertion sort using in-place algorithm.
-    insertion_sort_IP(arr, n);
-    cout << endl;
-    cout << "ARRAY AFTER SORTING USING IN-PLACE ALGORITHM : ";
-    // printing the array
-    pfun(arr, n);
-    cout << endl;
-    // calling insertiong sort using out-place algorithm.
-    insertion_sort_OP(arr2, 0, n - 1);
-    cout << "ARRAY AFTER SORTING USING OUT-PLACE ALGORITHM : ";
-    // printing the array
-    pfun(arr, n);
     cout << endl;
 }
 
+int main()
+{
+    // n=number of elements in the array
+    int n;
+    // taking number of elements as input from the user
+    cout << "Enter number of elements in array:";
+    cin >> n;
+    // array of size n
+    int arr1[n];
+    int arr2[n];
+    // taking array as input from the user
+    cout << "Enter elements of array:";
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        arr1[i] = a;
+        arr2[i] = a;
+    }
+    // printing array before sorting
+    cout << "\nARRAY before sorting:";
+    pfun(arr1, n);
+    cout << endl;
+    // sorting array using bubble sort and printing it
+    cout << "BUBBLE SORT" << endl;
+    bubble_sort(arr1, n);
+    cout << " . ARRAY After Sorting Using Bubble Sorting:";
+    pfun(arr1, n);
+    cout << endl;
+    // sorting array using insertion sort and printing it
+    cout << "SELECTION SORT" << endl;
+    selection_sort(arr2, n);
+    cout << " . ARRAY After Sorting Using Selection Sorting:";
+    pfun(arr2, n);
+}
 /*
-[ANS.3]
-In-place techniques have lesser space complexity but are difficult to
-apply in algorithm whereas out-place techniques are easy to aplly but
-increases the space complexity of algorithm.
-For exapmle if we want to reverse an array then its in-place algorithm
-will be swaping the first and last element of array until we reach the
-middle of array,If we want to solve the same problem using out-place
-algorithm we have to create one extra array of same size and copy
-elements of original array from back to the front of new array,this
-algorith increases the space complexity to O(n) as we have creaed an
-extra array.
-IN-PLACE algorithm are used in Bubble sort, Selection Sort, Insertion Sort, Heapsort.
-NOT IN-PLACE algorithm is used in Merge Sort, Because merge sort requires O(n) extra space.
+BONUS QUESTION
+TIME COMPLEXITY OF BUBBLE SORT
+   From the above code of BUBBLE SORT we can see that there are two
+   while loops which will we executed.First while loop will run n-1
+   times and second while loop will run n-2 times and so on till 1 time.
+   Time Complexity=(n-1)+(n-2)+(n-3)....+2+1=(n-1)(n)/2
+                  =O(n^2)
+TIME COMPLEXITY OF SELECTION SORT
+    In selection sort the main time consuming task is finding minimum
+    element from the array.We have to scan all the elements of the
+    array to find the minimum element.
+    With reference to the above code of SELECTION SORT,first we will
+    search for minimum element in array of size n,then in the same array of size n-1,and so on till array of size 1.
+    Time Complexity=(n)+(n-1)+(n-2)...1=(n)(n+1)/2
+                   =O(n^2)
 */
